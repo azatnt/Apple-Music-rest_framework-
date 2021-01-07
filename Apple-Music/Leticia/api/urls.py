576ae97ecf5d923cb6afa3aee1a685dbd4875550
pr1nce07 <58590243+pr1nce07@users.noreply.github.com>
2021-01-07 22:45:14 +0500

    Post request creating room

--- Apple-Music/Leticia/api/urls.py
+++ Apple-Music/Leticia/api/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 
 urlpatterns = [
-    path('room', RoomView.as_view()),
+    path('room', RoomView.as_view(), name='all_rooms_url'),
+    path('create-room', CreateRoomView.as_view(), name='create_room_url'),
 ]
