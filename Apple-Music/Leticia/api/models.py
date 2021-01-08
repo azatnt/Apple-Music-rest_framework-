2b7c0776aeb6351d79a3088bd4f142daaffa96ad
pr1nce07 <58590243+pr1nce07@users.noreply.github.com>
2021-01-08 16:54:56 +0500

    Api endpoints from React

--- Apple-Music/Leticia/api/models.py
+++ Apple-Music/Leticia/api/models.py
@@ -7,14 +7,19 @@ def generate_unique_code():
     length = 6
     while True:
         code = ''.join(random.choices(string.ascii_uppercase, k=length))
-        if Room.objects.filter(code = code).count() == 0:
+        if Room.objects.filter(code=code).count() == 0:
             break
+
     return code
 
 
 class Room(models.Model):
-    code = models.CharField(max_length=8, default="generate_unique_code", unique=True)
+    code = models.CharField(
+        max_length=8, default=generate_unique_code, unique=True)
     host = models.CharField(max_length=50, unique=True)
     guest_can_pause = models.BooleanField(default=False, null=False)
     votes_to_skip = models.IntegerField(null=False, default=2)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.code
--- Apple-Music/Leticia/api/urls.py
+++ Apple-Music/Leticia/api/urls.py
@@ -5,4 +5,5 @@ from .views import *
 urlpatterns = [
     path('room', RoomView.as_view(), name='all_rooms_url'),
     path('create-room', CreateRoomView.as_view(), name='create_room_url'),
+    path('get-room', GetRoom.as_view(), name='get_room_url'),
 ]
